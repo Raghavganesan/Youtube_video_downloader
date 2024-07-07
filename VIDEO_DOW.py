0ab55ba27c25c2f296210585cef49a08d8884ce5
Raghavganesan <159137537+Raghavganesan@users.noreply.github.com>
2024-07-07 15:13:43 +0530

    Add files via upload
    This is the python code for a YOUTUBE VIDEO DOWNLOADER

---
+++ VIDEO_DOW.py
@@ -0,0 +1,38 @@
+from pytube import YouTube  # type: ignore # Corrected import statement
+import tkinter as tk
+from tkinter import filedialog
+
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)  # Corrected class name
+        streams = yt.streams.filter(progressive=True, file_extension="mp4")
+        highest_res_stream = streams.get_highest_resolution()
+        highest_res_stream.download(output_path=save_path)
+        print("Video Downloaded Successfully!")
+
+    except Exception as e:
+        print(e)
+
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"Selected folder:{folder}")
+        
+    return folder
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+    
+    
+    video_url = input("enter the youtube url :")
+    save_dir = open_file_dialog()
+    
+    if save_dir:
+        print("Started download")
+        download_video(video_url,save_dir)
+    else:
+       print("Invalid save location.")
+    
